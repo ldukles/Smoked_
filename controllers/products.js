5cb9c1b363555c08bdbb78e6cd2bc8e5923ac9ac
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-30 12:24:33 -0400

    connecting seed data

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,26 @@
+// DEPENDENCIDES
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product.js');
 
+// SEED ROUTE
+const productSeed = require('../models/productSeed');
+
+// INDEX
 router.get('/', (req, res) => {
     res.render('/views/products/index.ejs');
 });
+
+// N
+
+// D
+
+// U
+
+// C
+
+// E
+
+// S
 
 module.exports = router;

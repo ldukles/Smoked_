528b557b77849bd863f469366b5916b76d7d46ed
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-30 15:03:16 -0400

    index and new page displaying

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
 // DEPENDENCIDES
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 const Product = require('../models/product.js');
 
@@ -8,19 +9,38 @@ const productSeed = require('../models/productSeed');
 
 // INDEX
 router.get('/', (req, res) => {
-    res.render('/views/products/index.ejs');
+    Product.find({}, (err, foundProducts) => {
+        res.render('products/index.ejs', {
+            products: foundProducts
+        });
+    })   
 });
 
 // N
+router.get('/new', (req, res) => {
+    res.render('products/new.ejs');
+});
 
 // D
 
 // U
 
 // C
+router.post('/', (req, res) => {
+    Product.create(req.body, (err, createdProduct) => {
+        res.redirect('/products');
+    });
+});
 
 // E
 
 // S
+router.get('/:id', (req, res) => {
+    Product.findById(req.params.id, (err, foundProduct) => {
+        res.render('/products/show.ejs', {
+            product: foundProduct
+        });
+    });
+});
 
 module.exports = router;
